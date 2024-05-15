6bbcd74d05b840468418e63a91f0ab9b97d2dad7
Michael McDuffee <mrmcduff@gmail.com>
2024-05-14 21:32:11 -0400

    adding event handlers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from textual import on
+from textual import on, events
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal
-from textual.widgets import Button, Footer, Header, Input, Log, Static
+from textual.widgets import Button, Footer, Header, Input, Log, RichLog, Static
 
 
 class ConsoleBSM(App):
@@ -16,14 +16,30 @@ class ConsoleBSM(App):
           Input(placeholder="Ticker", id="ticker", classes="input"),
           Button(label="Fetch OHLC", id="fetch", classes="button"),
         )
-        yield Static(classes="vspacer")
-        yield Log(max_lines=2, id="log", classes="log")
+        yield RichLog(classes="vspacer")
+        yield Log(max_lines=2, id="keylog", classes="keylog", auto_scroll=True)
+        yield Log(max_lines=2, id="log", classes="log", auto_scroll=True)
         yield Footer()
 
     @on(Button.Pressed, "#fetch")
     def handle_button_click(self) -> None:
         logout = self.query_one("#log", Log)
-        logout.write_line("Button was pressed")
+        text_holder = self.query_one("#ticker", Input)
+        logout.write_line(f"Button was pressed: {text_holder.value}")
+
+    @on(Input.Changed, "#ticker")
+    def handle_input_changed(self, event: Input.Changed) -> None:
+        logout = self.query_one("#log", Log)
+        logout.write_line(f"Input changed to {event.value}")
+
+    @on(Input.Submitted, "#ticker")
+    def handle_input_changed(self, event: Input.Submitted) -> None:
+        logout = self.query_one("#log", Log)
+        logout.write_line(f"Input submitted with {event.value}")
+
+    def on_key(self, event: events.Key ) -> None:
+        logout = self.query_one("#keylog", Log)
+        logout.write(event.character)
 
 
     def action_toggle_dark(self) -> None:
