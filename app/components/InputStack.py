0eeec8ab6ee24d36778a527b369c824a02ac67b4
Michael McDuffee <mrmcduff@gmail.com>
2024-05-16 10:23:58 -0400

    now with functional layout

---
+++ app/components/InputStack.py
@@ -0,0 +1,24 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.widget import Widget
+from textual.widgets import Button, Footer, Header, Input, Log, RichLog, Static
+
+
+class InputStack(Widget):
+
+  def compose(self) -> ComposeResult:
+    yield Input(placeholder="Ticker", id="ticker_input", valid_empty=False)
+    yield Input(placeholder="Date", id="date_input", valid_empty=False)
+
+  @on(Input.Submitted, "#ticker_input")
+  def handle_input_submitted(self, event: Input.Submitted) -> None:
+    ticker = self.query_one("#ticker_input", Input)
+    date_input = self.query_one("#date_input", Input)
+    if date_input.value == '':
+      date_input.focus()
+    elif not date_input.is_valid:
+      # send a message that can be passed to the logger
+      pass
+    else:
+      # simulate sending a message to the server
+      pass
--- app/main.py
+++ app/main.py
@@ -1,48 +1,76 @@
-from textual import on, events
+import asyncio
+from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal
+from textual.containers import Grid
 from textual.widgets import Button, Footer, Header, Input, Log, RichLog, Static
 
-
 class ConsoleBSM(App):
-    """A Textual app to check equity prices and volatilities."""
-    CSS_PATH = "styles.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("x", "exit_app", "Exit App")]
+  """A Textual app to check equity prices and volatilities."""
+  CSS_PATH = "styles.tcss"
+  BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("x", "exit_app", "Exit App")]
 
-    def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
-        yield Header()
-        yield Horizontal(
-          Input(placeholder="Ticker", id="ticker", classes="input"),
-          Button(label="Fetch OHLC", id="fetch", classes="button"),
-        )
-        yield RichLog(classes="vspacer")
-        yield Log(max_lines=2, id="keylog", classes="keylog", auto_scroll=True)
-        yield Log(max_lines=2, id="log", classes="log", auto_scroll=True)
-        yield Footer()
+  def compose(self) -> ComposeResult:
+    """Create child widgets for the app."""
+    yield Header()
+    yield Grid(
+      Input(placeholder="Ticker", id="ticker", classes="box"),
+      Button(name="OHLC", id="ohlc", label="OHLC", classes="box"),
+      Button(name="OPT", id="opt", label="OPT", classes="box"),
+      Input(placeholder="Date", id="date-input", classes="box"),
+      Static(classes="box"),
+      Button(name="CALC", id="calc", label="CALC", classes="box"),
+      Button(name="CLEAR", id="clear", label="CLEAR", classes="box"),
+      RichLog(id="user-output", classes="box")
+    )
+    yield Footer()
 
-    @on(Button.Pressed, "#fetch")
-    def handle_button_click(self) -> None:
-        logout = self.query_one("#log", Log)
-        text_holder = self.query_one("#ticker", Input)
-        logout.write_line(f"Button was pressed: {text_holder.value}")
+  @on(Button.Pressed, "#fetch")
+  async def handle_button_click(self) -> None:
+    logout = self.query_one("#log", Log)
+    btn = self.query_one("#fetch")
+    btn.loading = True
+    text_holder = self.query_one("#ticker", Input)
+    logout.write_line("Starting task")
+    self.log_out('going to sleep')
+    await asyncio.sleep(3)
+    self.log_out('waking up')
 
-    @on(Input.Changed, "#ticker")
-    def handle_input_changed(self, event: Input.Changed) -> None:
-        logout = self.query_one("#log", Log)
-        logout.write_line(f"Input changed to {event.value}")
+    await self.fetch_ohlc_data("hello")
+    logout.write_line("Task complete")
+    btn.loading = False
+    logout.write_line(f"Button was pressed: {text_holder.value}")
 
-    @on(Input.Submitted, "#ticker")
-    def handle_input_changed(self, event: Input.Submitted) -> None:
-        logout = self.query_one("#log", Log)
-        logout.write_line(f"Input submitted with {event.value}")
+  def log_out(self, something: str) -> None:
+    logout = self.query_one("#log", Log)
+    logout.write_line(something)
 
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
 
-    def action_exit_app(self) -> None:
-        self.exit()
+  @on(Input.Changed, "#ticker")
+  def handle_input_changed(self, event: Input.Changed) -> None:
+    logout = self.query_one("#log", Log)
+    logout.write_line(f"Input changed to {event.value}")
+
+  @on(Input.Submitted, "#ticker")
+  def handle_input_changed(self, event: Input.Submitted) -> None:
+    logout = self.query_one("#log", Log)
+    logout.write_line(f"Input submitted with {event.value}")
+
+  async def fetch_ohlc_data(self, ticker: str) -> None:
+    btn = self.query_one("#fetch")
+    logout = self.query_one("#log", Log)
+    logout.write_line("Starting second method")
+    logout.refresh()
+    await asyncio.sleep(3)
+    logout.write_line(f"ending second method {ticker}")
+    btn.loading = False
+    self.refresh()
+
+  def action_toggle_dark(self) -> None:
+    """An action to toggle dark mode."""
+    self.dark = not self.dark
+
+  def action_exit_app(self) -> None:
+    self.exit()
 
 if __name__ == "__main__":
     app = ConsoleBSM()
