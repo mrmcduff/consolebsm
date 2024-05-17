86ee6477208496516b92803f0656d34aebed80f6
Michael McDuffee <mrmcduff@gmail.com>
2024-05-17 10:32:56 -0400

    connecting to polygon and fetching a day of data

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import asyncio
+import httpx
+import os
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Grid
 from textual.widgets import Button, Footer, Header, Input, Log, RichLog, Static
 from utils.date_tools import default_date_str, parse_date, format_date
+from dotenv import load_dotenv
+from rich import json
+
+load_dotenv()
+polygon_key = os.getenv('POLYGON_IO_API_KEY')
+polygon_base = "https://api.polygon.io"
 
 class ConsoleBSM(App):
   """A Textual app to check equity prices and volatilities."""
@@ -38,27 +46,48 @@ class ConsoleBSM(App):
   async def handle_ohlc_click(self) -> None:
     await self.fetch_ohlc_data()
 
-
-  # @on(Input.Changed, "#ticker")
-  # def handle_input_changed(self, event: Input.Changed) -> None:
-  #   logout = self.query_one("#log", Log)
-  #   logout.write_line(f"Input changed to {event.value}")
-
   @on(Input.Submitted, "#ticker")
   async def handle_ticker_submitted(self) -> None:
     await self.fetch_ohlc_data()
 
   async def fetch_ohlc_data(self) -> None:
-    self.set_ohlc_disabled(True)
     if self.has_valid_fields():
       self.log_out('Fields are valid. I am prepared to fetch')
     else:
       self.log_out('Fields are not valid. Fetcher out.')
-    self.log_out('going to sleep')
-    await asyncio.sleep(3)
+      return
+
+    self.set_ohlc_disabled(True)
+    ticker = self.get_input_ticker()
+    datestr = self.get_input_date()
+
+    formatted_json = await self.make_api_call(ticker=ticker, datestr=datestr)
+    if formatted_json is None:
+      self.log_out('Something failed')
+    else:
+      self.log_json(formatted_json)
     self.log_out('waking up')
-    # self.log_out(f"Ticker has value {ticker}")
     self.set_ohlc_disabled(False)
+
+  async def make_api_call(self, ticker: str, datestr: str) -> json.JSON | None:
+    url = f"https://api.polygon.io/v1/open-close/{ticker.upper()}/{datestr}?adjusted=true"
+    headers = {
+      "Authorization": f"Bearer {polygon_key}"
+    }
+    self.log_out('about to create the client')
+    async with httpx.AsyncClient() as client:
+      try:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        self.log_out('Managed to get a positive response from the server')
+        formatted = json.JSON.from_data(response.json())
+        return formatted
+      except httpx.RequestError as exc:
+        self.log_out(f"An error occurred while requesting {exc.request.url!r}.")
+      except httpx.HTTPStatusError as exc:
+        self.log_out(f"Error response {exc.response.reason_phrase} while requesting {exc.request.url!r}.")
+      except Exception as exc:
+        self.log_out(f"An unexpected error occurred: {exc}")
 
   def get_input_date(self) -> str | None:
     date_input = self.query_one("#date-input", Input)
@@ -71,6 +100,10 @@ class ConsoleBSM(App):
   def log_out(self, something: str) -> None:
     logout = self.query_one("#user-output", RichLog)
     logout.write(something)
+
+  def log_json(self, data: json.JSON) -> None:
+    logout = self.query_one("#user-output", RichLog)
+    logout.write(data)
 
   def get_input_ticker(self) -> str:
     ticker_input = self.query_one("#ticker", Input)
